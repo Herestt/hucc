01cdee80a86bda626afb5fab3e58e4c67dc7f6c8
Herestt <herestt@gmail.com>
2015-04-22 19:31:56 +0200

    Buffers class specification.

---
+++ src/main/java/com/herestt/common/nio/Buffers.java
@@ -0,0 +1,79 @@
+package com.herestt.common.nio;
+
+import java.nio.ByteBuffer;
+
+public class Buffers {
+
+	/*** Byte Data Type Processing. ***/
+	public static short getUnsignedByte(ByteBuffer src) {
+		// TODO - Herestt : implementation.
+		return 0;
+	}
+	
+	public static void putUnsignedByte(ByteBuffer dst, short value) {
+		// TODO - Herestt : implementation.
+	}
+	
+	public static void getUnsignedByte(ByteBuffer src, short[] dst) {
+		// TODO - Herestt : implementation.
+	}
+	
+	public static void putUnsignedByte(ByteBuffer dst, short[] src) {
+		// TODO - Herestt : implementation.
+	}
+	
+	public static void getUnsignedByte(ByteBuffer src, ByteBuffer dst) {
+		// TODO - Herestt : implementation.
+	}
+	
+	public static void putUnsignedByte(ByteBuffer dst, ByteBuffer src) {
+		// TODO - Herestt : implementation.
+	}
+	
+	public static short getUnsignedByte(ByteBuffer src, int index) {
+		// TODO - Herestt : implementation.
+		return 0;
+	}
+	
+	public static void putUnsignedByte(ByteBuffer dst, int index, short value) {
+		// TODO - Herestt : implementation.
+	}
+	
+	/*** Short Data Type Processing. ***/
+	public static int getUnsignedShort(ByteBuffer src) {
+		// TODO - Herestt : implementation.
+		return 0;
+	}
+	
+	public static void putUnsignedShort(ByteBuffer dst, int value) {
+		// TODO - Herestt : implementation.
+	}
+	
+	public static int getUnsignedShort(ByteBuffer src, int index) {
+		// TODO - Herestt : implementation.
+		return 0;
+	}
+	
+	public static void putUnsignedShort(ByteBuffer dst, int index, int value) {
+		// TODO - Herestt : implementation.
+	}
+	
+	/*** Integer Data Type Processing. ***/
+	public static long getUnsignedInt(ByteBuffer src) {
+		// TODO - Herestt : implementation.
+		return 0;
+	}
+	
+	public static void putUnsignedInt(ByteBuffer dst, long value) {
+		// TODO - Herestt : implementation.
+	}
+	
+	public static long getUnsignedInt(ByteBuffer src, int index) {
+		// TODO - Herestt : implementation.
+		return 0;
+	}
+	
+	public static void putUnsignedInt(ByteBuffer dst, int index, long value) {
+		// TODO - Herestt : implementation.
+	}
+}
