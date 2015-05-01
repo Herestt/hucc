184b1fe38e48a8d56518ab1aa6ad8ca08c08472c
Herestt <herestt@gmail.com>
2015-05-01 13:16:10 +0200

    TgaImageReaderSpi implementation.

--- src/main/java/com/herestt/common/imageio/plugins/tga/TgaImageReaderSpi.java
+++ src/main/java/com/herestt/common/imageio/plugins/tga/TgaImageReaderSpi.java
@@ -5,25 +5,71 @@ import java.util.Locale;
 
 import javax.imageio.ImageReader;
 import javax.imageio.spi.ImageReaderSpi;
+import javax.imageio.stream.ImageInputStream;
 
 public class TgaImageReaderSpi extends ImageReaderSpi {
 	
+	private static final String vendorName = "Herestt";
+	
+	private static final String version = "1.0";
+	
+	private static final String[] names = {"tga", "TGA", "TARGA"};
+	
+	private static final String[] suffixes = {"tga"};
+			
+	private static final String[] MIMETypes = {"image/tga", "image/x-tga", "image/targa", "image/x-targa"};
+
+	private static final String readerClassName = "com.herestt.common.imageio.plugins.tga.TgaImageReader";
+	
+	private static final String[] writerSpiNames = {
+		"com.herestt.common.imageio.plugins.tga.TgaImageWriterSpi"
+	};
+	
+	private static final String nativeImageMetadataFormatClassName =
+			"com.herestt.common.imageio.plugins.tga.TgaMetadataFormat";
+	
+	private static final String signature = "TRUEVISION-XFILE";
+	
+	public TgaImageReaderSpi() {
+		super(vendorName,
+				version,
+				names,
+				suffixes,
+				MIMETypes,
+				readerClassName,
+				new Class[] { ImageInputStream.class },
+				writerSpiNames,
+				false,
+				null, null,
+				null, null,
+				true,
+				TgaMetadata.nativeImageMetadataFormatName,
+				nativeImageMetadataFormatClassName,
+				null, null);
+	}
+	
 	@Override
 	public boolean canDecodeInput(Object source) throws IOException {
-		// TODO Auto-generated method stub
+		if(!(source instanceof ImageInputStream))
+			return false;
+		ImageInputStream stream = (ImageInputStream) source;
+		byte[] signatureBytes = new byte[signature.length()]; 
+		long pos = stream.length() - 17 - 1;	// Signature starting offset.
+		stream.seek(pos);
+		stream.readFully(signatureBytes);
+		if(new String(signatureBytes).equals(signature))
+			return true;
 		return false;
 	}
 
 	@Override
 	public ImageReader createReaderInstance(Object extension)
 			throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		return new TgaImageReader(this);
 	}
 
 	@Override
 	public String getDescription(Locale locale) {
-		// TODO Auto-generated method stub
-		return null;
+		return "Standard TGA image reader.";
 	}
 }
